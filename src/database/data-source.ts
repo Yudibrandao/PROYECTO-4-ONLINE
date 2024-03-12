646d0ce0aa497ddbf7a94b289ff90cbb2ad6a669
´Yudith´ <yudi@geeksacademy.es>
2024-03-12 21:34:47 +0100

    estudiotatuajes

---
+++ src/database/data-source.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+
+export const dataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "estudio",
+    entities: [],
+    migrations: [], 
+
+})
